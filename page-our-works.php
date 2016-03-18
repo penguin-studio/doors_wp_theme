d77ece46471493e5ac68491047c23ccb39f8c2ff
Maksim Rudenko <rudenko.programmer@gmail.com>
2016-03-18 23:52:33 +0200

    Обновления с сервера

--- page-our-works.php
+++ page-our-works.php
@@ -5,7 +5,7 @@ Template Name: Шаблон страницы каталога
 ?>
 <?php get_header();?>
 <section class="main-content">
-
+    
 	<div class="container">
          <?php
             get_template_part('content/content-our-works');
