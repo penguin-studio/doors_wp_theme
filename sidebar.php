55faaee27e52adb06f10055542dcc46c38b97466
Polomkin A <polomkinanton@gmail.com>
2016-03-18 10:51:13 +0200

    добавление верстки фильтра

--- sidebar.php
+++ sidebar.php
@@ -10,4 +10,21 @@
 			    <? wp_nav_menu(array('menu' => 'aside-menu', 'sub' => 'aside-menu')); ?>
 
 			</div>
+			<div class="filter">
+                <form method="post">
+                    <p>Цена</p>
+                    <div class="price-loop-wrapp">
+                        <label>от<input type="text"></label>
+                        <label>до<input type="text"></label>
+                    </div>
+                    <select class="choice-drop">
+                        <option>Производитель</option>
+                        <option></option>
+                        <option></option>
+                    </select>
+
+                </form>
+
+
+            </div>
 		</aside>
